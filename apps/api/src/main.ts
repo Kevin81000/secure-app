3c588dec498e67e6b8b80c5e2c0e774e3dae0d06
Kevin81000 <kevin.tyler8100@gmail.com>
2025-12-10 23:03:36 -0600

    initial

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,12 +1,5 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { App } from './app/app';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  app.setGlobalPrefix('api');
-  await app.listen(process.env.PORT || 3000);
-  console.log(`Server running on http://localhost:${process.env.PORT || 3000}/api`);
-}
-bootstrap();
+bootstrapApplication(App, appConfig).catch((err) => console.error(err));
--- client/src/app/user-edit/user-edit.component.ts
+++ client/src/app/user-edit/user-edit.component.ts
@@ -1,25 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+
 import { User } from '../user.model';
-import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
-  selector: 'app-user-edit',
-  templateUrl: './user-edit.component.html',
+    selector: 'app-user-edit',
+    standalone: true,
+    imports: [
+        CommonModule,
+        FormsModule,
+        RouterModule
+    ],
+    templateUrl: './user-edit.component.html',
+    styleUrls: ['./user-edit.component.scss']
 })
 export class UserEditComponent {
-  user: User = { id: 0, name: '', email: '' };
+    user: User = { id: 0, name: '', email: '' };
 
-  constructor(
-    private route: ActivatedRoute,
-    private router: Router
-  ) {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.user.id = id;
-  }
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router
+    ) {
+        const id = Number(this.route.snapshot.paramMap.get('id'));
+        this.user.id = id || 0;
+    }
 
-  submit() {
-    console.log('Updating user:', this.user);
-    // TODO: PUT to backend later
-    this.router.navigate(['/users']);
-  }
+    submit() {
+        console.log('Updating user:', this.user);
+        // TODO: call your NestJS backend here later
+        this.router.navigate(['/users']);
+    }
 }
